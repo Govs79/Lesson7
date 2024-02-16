90af8536bfbdea1caa77faaf4c044b4b6f698637
Victor <govs79@outlook.com>
2024-02-16 16:00:36 +0300

    Задача 3

--- Program.cs
+++ Program.cs
@@ -1,17 +1,42 @@
 ﻿// Задача 1
-void ShowNumbers (int start, int end)
+// void ShowNumbers (int start, int end)
+// {
+//     if (start == end)
+//     {
+//         Console.WriteLine (start);
+//         return;
+//     }
+//     Console.WriteLine (start + " ");
+//     ShowNumbers (start + 1, end);
+// }
+
+// Console.Write("Введите начальное число: ");
+// int M = Convert.ToInt32(Console.ReadLine());
+// Console.Write ("Введите конечное число: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// ShowNumbers (M, N);
+
+// Задача 2
+
+// Задача 3
+void PrintReverseArray (int[] array, int j)
 {
-    if (start == end)
+    if (j == 0)
     {
-        Console.WriteLine (start);
+        Console.Write (array[j] + " ");
         return;
     }
-    Console.WriteLine (start + " ");
-    ShowNumbers (start + 1, end);
-}
+    Console.Write (array[j] + " ");
 
-Console.Write("Введите начальное число: ");
-int M = Convert.ToInt32(Console.ReadLine());
-Console.Write ("Введите конечное число: ");
+    PrintReverseArray(array, j - 1);
+    }
+Console.Write("Задайте размер массива: ");
 int N = Convert.ToInt32(Console.ReadLine());
-ShowNumbers (M, N);
+    int [] numbers = new int [N];
+Console.Write("Задайте массив из чисел: ");
+for (int i = 0; i < 10; i++) 
+{
+numbers[i] = Convert.ToInt32(Console.ReadLine());
+}
+int jm = numbers.Length-1;
+PrintReverseArray(numbers, jm);
