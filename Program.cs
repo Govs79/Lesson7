45f9c67e8c5e6e1bb8e91dff6ed20bf2e510a80c
Victor <govs79@outlook.com>
2024-02-16 15:05:04 +0300

    Задание 1

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 1
+void ShowNumbers (int start, int end)
+{
+    if (start == end)
+    {
+        Console.WriteLine (start);
+        return;
+    }
+    Console.WriteLine (start + " ");
+    ShowNumbers (start + 1, end);
+}
+
+Console.Write("Введите начальное число: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите конечное число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+ShowNumbers (M, N);
