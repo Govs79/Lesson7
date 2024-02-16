1e5afb735cde57ec27ad7bc1432a78b283899e3c
Victor <govs79@outlook.com>
2024-02-16 16:37:01 +0300

    Задача 2

--- Program.cs
+++ Program.cs
@@ -17,26 +17,45 @@
 // ShowNumbers (M, N);
 
 // Задача 2
+int FunctionAkkerman (int n, int m)
+{
+    if (n==0)
+    {
+        return m+1;
+    }
+    else if (m==0)
+    {
+        return FunctionAkkerman(n-1, 1);
+    }
+    else {
+        return FunctionAkkerman(n-1, FunctionAkkerman(n, m-1));
+    }
+}
+Console.Write("Задайте параметр n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Задайте параметр m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Функция Аккермана равна {FunctionAkkerman(n, m)}");
 
 // Задача 3
-void PrintReverseArray (int[] array, int j)
-{
-    if (j == 0)
-    {
-        Console.Write (array[j] + " ");
-        return;
-    }
-    Console.Write (array[j] + " ");
+// void PrintReverseArray (int[] array, int j)
+// {
+//     if (j == 0)
+//     {
+//         Console.Write (array[j] + " ");
+//         return;
+//     }
+//     Console.Write (array[j] + " ");
 
-    PrintReverseArray(array, j - 1);
-    }
-Console.Write("Задайте размер массива: ");
-int N = Convert.ToInt32(Console.ReadLine());
-    int [] numbers = new int [N];
-Console.Write("Задайте массив из чисел: ");
-for (int i = 0; i < 10; i++) 
-{
-numbers[i] = Convert.ToInt32(Console.ReadLine());
-}
-int jm = numbers.Length-1;
-PrintReverseArray(numbers, jm);
+//     PrintReverseArray(array, j - 1);
+//     }
+// Console.Write("Задайте размер массива: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+//     int [] numbers = new int [N];
+// Console.Write("Задайте массив из чисел: ");
+// for (int i = 0; i < 10; i++) 
+// {
+// numbers[i] = Convert.ToInt32(Console.ReadLine());
+// }
+// int jm = numbers.Length-1;
+// PrintReverseArray(numbers, jm);
